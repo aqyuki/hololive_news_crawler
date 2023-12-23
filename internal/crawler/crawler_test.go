09a75f3dfd93efe72879e7304e646859ee700306
aqyuki <yksk171118@gmail.com>
2023-12-23 22:39:06 +0900

    test: add test case for crawler

--- internal/crawler/crawler_test.go
+++ internal/crawler/crawler_test.go
@@ -1,10 +1,43 @@
 package crawler
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+// successURLExtractorMock is a mock of URLExtractor
+type successURLExtractorMock struct{}
+
+// ExtractURLs is a mock of ExtractURLs
+func (e *successURLExtractorMock) ExtractURLs(url string) ([]string, error) {
+	return []string{url}, nil
+}
+
+// errorURLExtractorMock is a mock of URLExtractor
+type errorURLExtractorMock struct{}
+
+// ExtractURLs is a mock of ExtractURLs
+func (e *errorURLExtractorMock) ExtractURLs(url string) ([]string, error) {
+	return nil, errors.New("mock")
+}
+
+type successExtractorMock struct{}
+
+func (e *successExtractorMock) Scrape(url string) (*Site, error) {
+	return &Site{
+		URL:         url,
+		Title:       "title",
+		Description: "description",
+	}, nil
+}
+
+type errorExtractorMock struct{}
+
+func (e *errorExtractorMock) Scrape(url string) (*Site, error) {
+	return nil, errors.New("mock")
+}
 
 func TestNewCrawler(t *testing.T) {
 	t.Parallel()
@@ -23,5 +56,65 @@ func TestNewCrawler(t *testing.T) {
 		mock := struct{ Extractor }{}
 		actual := NewCrawler(&mock)
 		assert.NotNil(t, actual)
+	})
+}
+
+func TestCrawler_SetURLExtractor(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return error if url extractor is nil", func(t *testing.T) {
+		t.Parallel()
+
+		c := NewCrawler(&successExtractorMock{})
+		err := c.SetURLExtractor(nil)
+		assert.ErrorIs(t, err, ErrURLExtractorIsNil)
+	})
+
+	t.Run("should be success if url extractor is not nil", func(t *testing.T) {
+		t.Parallel()
+
+		c := NewCrawler(&successExtractorMock{})
+		err := c.SetURLExtractor(&successURLExtractorMock{})
+		assert.NoError(t, err)
+		assert.EqualValues(t, &successURLExtractorMock{}, c.urlExtractor)
+	})
+}
+
+func TestCrawler_Crawl(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should be success", func(t *testing.T) {
+		t.Parallel()
+
+		c := NewCrawler(&successExtractorMock{})
+		c.SetURLExtractor(&successURLExtractorMock{})
+		actual, err := c.Crawl("https://example.com")
+		assert.NoError(t, err)
+		assert.EqualValues(t, []Site{
+			{
+				URL:         "https://example.com",
+				Title:       "title",
+				Description: "description",
+			},
+		}, actual)
+	})
+
+	t.Run("should return error if url extractor is nil", func(t *testing.T) {
+		t.Parallel()
+
+		c := NewCrawler(&errorExtractorMock{})
+		actual, err := c.Crawl("https://example.com")
+		assert.Error(t, err)
+		assert.Nil(t, actual)
+	})
+
+	t.Run("should return error if url extractor returns error", func(t *testing.T) {
+		t.Parallel()
+
+		c := NewCrawler(&successExtractorMock{})
+		c.SetURLExtractor(&errorURLExtractorMock{})
+		actual, err := c.Crawl("https://example.com")
+		assert.Error(t, err)
+		assert.Nil(t, actual)
 	})
 }
