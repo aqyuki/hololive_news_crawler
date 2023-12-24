4ceeb567c296f31f2fbd8c1e2cae02bf41ebd559
aqyuki <yksk171118@gmail.com>
2023-12-24 11:02:29 +0900

    fix: fix failed to collect urls

--- internal/extractor/hololive/url_extractor.go
+++ internal/extractor/hololive/url_extractor.go
@@ -29,14 +29,13 @@ func (e *HololiveNewsURLExtractor) ExtractURLs(url string) ([]string, error) {
 	var targetURL string = url
 
 	for {
-
 		urls, err := e.extractURLs(targetURL)
 		if err != nil {
 			return nil, fmt.Errorf("failed to extract urls: %w", err)
 		}
 		collectedURLs = append(collectedURLs, urls...)
 
-		nextPageURL, err := e.extractNextPage(url)
+		nextPageURL, err := e.extractNextPage(targetURL)
 		if errors.Is(err, ErrNotFoundNext) {
 			break
 		} else if err != nil {
@@ -56,11 +55,8 @@ func (e *HololiveNewsURLExtractor) extractURLs(url string) ([]string, error) {
 	var collector *colly.Collector = colly.NewCollector()
 	collector.AllowedDomains = allowedDomains
 
-	collector.OnHTML("#container > main > div.in_news.fade_b.isPlay_b > div > ul > li", func(h *colly.HTMLElement) {
-		href, exist := h.DOM.Find("a[href]").Attr("href")
-		if !exist {
-			return
-		}
+	collector.OnHTML("ul.news_list > li", func(h *colly.HTMLElement) {
+		href := h.DOM.Find("a").AttrOr("href", "")
 		collectedURLs = append(collectedURLs, href)
 	})
 
@@ -87,6 +83,10 @@ func (e *HololiveNewsURLExtractor) extractNextPage(url string) (string, error) {
 
 	if err := collector.Visit(url); err != nil {
 		return "", fmt.Errorf("failed to visit: %w", err)
+	}
+
+	if next == "" {
+		return "", ErrNotFoundNext
 	}
 	return next, nil
 }
