e3dad975a614f1440427a9842b2760ca3a516662
aqyuki <yksk171118@gmail.com>
2023-12-24 11:13:35 +0900

    test: add test case for context

--- pkg/logging/logging_test.go
+++ pkg/logging/logging_test.go
@@ -1,6 +1,7 @@
 package logging
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +12,15 @@ func TestNewLogger(t *testing.T) {
 
 	actual := NewLogger()
 	assert.NotNil(t, actual)
+}
+
+func TestContext(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	logger := FromContext(ctx)
+	assert.NotNil(t, logger)
+
+	ctx = WithLogger(ctx, logger)
+	assert.Equal(t, logger, FromContext(ctx))
 }
