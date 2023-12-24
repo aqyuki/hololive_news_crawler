16847f0fbc90a7354c67b0168a9568d48d272e9e
aqyuki <yksk171118@gmail.com>
2023-12-24 11:13:51 +0900

    feat: add function to use logger from context

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -1,11 +1,16 @@
 package logging
 
 import (
+	"context"
 	"log/slog"
 	"time"
 
 	"github.com/m-mizutani/clog"
 )
+
+type contextKey string
+
+const loggerKey = contextKey("logger")
 
 // NewLogger creates a new logger
 func NewLogger() *slog.Logger {
@@ -16,4 +21,17 @@ func NewLogger() *slog.Logger {
 		clog.WithPrinter(clog.PrettyPrinter),
 	)
 	return slog.New(handler)
+}
+
+// WithLogger returns a new context with the given logger
+func WithLogger(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+
+// FromContext returns the logger from the given context
+func FromContext(ctx context.Context) *slog.Logger {
+	if logger, ok := ctx.Value(loggerKey).(*slog.Logger); ok {
+		return logger
+	}
+	return NewLogger()
 }
